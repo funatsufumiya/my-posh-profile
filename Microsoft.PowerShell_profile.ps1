ccefb931b9290af77e3fc1d5b815fa5c019a3e9d
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2024-01-15 21:01:53 +0900

    add ln

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,6 +3,15 @@ Set-Alias vim nvim
 Set-Alias vi nvim
 Function mbash { nvim $PROFILE }
 
+function ln([switch] $s, [string] $filePath, [string] $symlink) {
+    if ($s) {
+        New-Item -ItemType SymbolicLink -Value $filePath -Path $symlink | Out-Null
+    }
+    else {
+        New-Item -ItemType HardLink -Value $filePath -Path $symlink | Out-Null
+    }
+}
+
 # oh-my-posh theme
 oh-my-posh init pwsh --config $env:POSH_THEMES_PATH/robbyrussell.omp.json | Invoke-Expression
 
