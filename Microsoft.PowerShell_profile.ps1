3547cb9973eaca0692234a335d9c9176abce0e6e
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2024-01-15 17:08:38 +0900

    add nvim

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,8 @@
 Set-Alias touch New-Item # touch command like linux
+Set-Alias vim nvim
+Set-Alias vi nvim
+Function mbash { nvim $PROFILE }
+
 # oh-my-posh theme
 oh-my-posh init pwsh --config $env:POSH_THEMES_PATH/robbyrussell.omp.json | Invoke-Expression
 
